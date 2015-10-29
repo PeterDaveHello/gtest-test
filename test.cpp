4b70ae0f1793e2d75534fb9f70d7f67c5e254e6c
Peter Dave Hello <hsu@peterdavehello.org>
2015-10-30 01:20:34 +0800

    add new test to test triangle type

--- test.cpp
+++ test.cpp
@@ -5,12 +5,19 @@ bool isTriangle(int a, int b, int c) {
     return (!((a <= 0 || b <= 0 || c <= 0) || ( a + b <= c || c + b <= a || c + a <= b)));
 }
 
+int triangleType(int a, int b, int c) {
+}
+
 TEST(isValid, invalidTriangle) {
     EXPECT_EQ(isTriangle(3, 4, 1), false);
 }
 
 TEST(isValid, validTriangle) {
     EXPECT_EQ(isTriangle(3, 4, 5), true);
+}
+
+TEST(type, equilateralTriangle) {
+    EXPECT_EQ(triangleType(3, 3, 3), 0) << "type of 3, 3, 3 is Equilateral";
 }
 
 int main(int argc, char* argv[]) {
